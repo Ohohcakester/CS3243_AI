48c376e6407a17581f354df0af3d232edd0e7222
ohohcakester <ohohgit@gmail.com>
2015-02-18 18:13:06 +0800

    Stupid algorithm

--- src/OhTestPlayer.java
+++ src/OhTestPlayer.java
@@ -3,39 +3,71 @@ import java.util.Random;
 
 
 public class OhTestPlayer {
-    private Random rand;
+    private Random rand = new Random();
+    private int lastPiece;
+    private int lastOrient;
+    private int lastPos;
+    private int current;
     
     public OhTestPlayer() {
-        rand = new Random();
+        current = 0;
+        lastPiece = 0;
     }
 
 	//implement this function to have a working system
-	public int pickMove(State s, int[][] legalMoves) {
-	    System.out.println(s.getNextPiece());
-	    System.out.println(Arrays.deepToString(legalMoves));
-
-		return 3;
+	public int[] pickMove(State s, int[][] legalMoves) {
+        int piece = s.getNextPiece();
+        int orient = chooseOrient(piece);
+	    int position = State.COLS-1;
+	    
+	    if (piece != 1 || rand.nextInt(3) != 0) {
+    	    current+=width(lastOrient, lastPiece);
+    	    if (current >= posLimit(orient, piece)) {
+    	        current = 0;
+    	    }
+    	    position = current;
+	    }
+	    
+	    lastPiece = piece;
+        lastOrient = orient;
+        lastPos = position;
+	    return new int[]{orient, position};
 	}
+    
+    
+    private int chooseOrient(int piece) {
+        final int[] o = new int[]{0,0,0,0,0,1,1};
+        return o[piece];
+    }
+    
+    
+    private int posLimit(int orient, int piece) {
+        return State.COLS-width(orient, piece);
+    }
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	private static int width(int orient, int piece) {
+	   final int[][] w = new int[][]{
+                new int[]{2},
+                new int[]{1,4},
+                new int[]{2,3,2,3},
+                new int[]{2,3,2,3},
+                new int[]{2,3,2,3},
+                new int[]{3,2},
+                new int[]{3,2}
+	    };
+	    return w[piece][orient];
+	}
 	
 	public static void main(String[] args) {
 		State s = new State();
 		new TFrame(s);
 		OhTestPlayer p = new OhTestPlayer();
 		while(!s.hasLost()) {
-			s.makeMove(0, p.pickMove(s,s.legalMoves()));
+			s.makeMove(p.pickMove(s,s.legalMoves()));
 			s.draw();
 			s.drawNext(0,0);
 			try {
-				Thread.sleep(3000);
+				Thread.sleep(300);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
