5b68b4316ad2e7476c2f9a93eafe731c043280da
ohohcakester <ohohgit@gmail.com>
2015-02-23 15:02:57 +0800

    FeatureFunctions class

---
+++ src/FeatureFunctions.java
@@ -0,0 +1,12 @@
+
+public class FeatureFunctions {
+
+    public static float exampleFeature(State s) {
+        return 4;
+    }
+
+    public static float maximumColumnHeight(int orient, int position, State s) {
+        return 1;
+    }
+    
+}
